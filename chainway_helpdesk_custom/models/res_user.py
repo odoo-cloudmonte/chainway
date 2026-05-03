a13b8cc49f86a9d4401deb377c8c97229cce831a
Aman Mishra <aman.mishra@cloudmonte.com>
2026-05-03 18:05:00 +0530

    update

--- chainway_helpdesk_custom/models/res_user.py
+++ chainway_helpdesk_custom/models/res_user.py
@@ -11,10 +11,10 @@ class ResUsers(models.Model):
         help="Allow user to see Device UI / Views"
     )
 
-    mis_access = fields.Selection([
-        ('yes', 'Yes'),
-        ('no', 'No')
-    ], string="MIS Access", default='no')
+    # mis_access = fields.Selection([
+    #     ('yes', 'Yes'),
+    #     ('no', 'No')
+    # ], string="MIS Access", default='no')
 
     # @api.model
     # def create(self, vals):
@@ -55,23 +55,23 @@ class ResUsers(models.Model):
                     new_groups = [gid for gid in current_groups if gid != group.id]
                     user.group_ids = [(6, 0, new_groups)]
 
-    def write(self, vals):
-        res = super().write(vals)
-        group = self.env.ref('chainway_helpdesk_custom.device_inventory_mis')
+    # def write(self, vals):
+    #     res = super().write(vals)
+    #     group = self.env.ref('chainway_helpdesk_custom.device_inventory_mis')
 
-        for user in self:
-            current_groups = user.group_ids.ids
-            if user.mis_access == 'yes':
-                if group.id not in current_groups:
-                    user.group_ids = [(6, 0, current_groups + [group.id])]
-                # user.groups_id = [(4, group.id)]
-            else:
-                # user.groups_id = [(3, group.id)].
-                if group.id in current_groups:
-                    new_groups = [gid for gid in current_groups if gid != group.id]
-                    user.group_ids = [(6, 0, new_groups)]
+    #     for user in self:
+    #         current_groups = user.group_ids.ids
+    #         if user.mis_access == 'yes':
+    #             if group.id not in current_groups:
+    #                 user.group_ids = [(6, 0, current_groups + [group.id])]
+    #             # user.groups_id = [(4, group.id)]
+    #         else:
+    #             # user.groups_id = [(3, group.id)].
+    #             if group.id in current_groups:
+    #                 new_groups = [gid for gid in current_groups if gid != group.id]
+    #                 user.group_ids = [(6, 0, new_groups)]
 
-        return res
+    #     return res
 
 
     
