bf1c913b77adc22f37c4ae22d8bef1ff94643cab
Aman Mishra <aman.mishra@cloudmonte.com>
2026-05-08 18:15:16 +0530

    client changes

--- chainway_helpdesk_custom/models/helpdesk.py
+++ chainway_helpdesk_custom/models/helpdesk.py
@@ -24,7 +24,7 @@ class TicketHelpdesk(models.Model):
     company_name = fields.Char(string="Company Name")
     company_address = fields.Char(string="Company Address")
     contact = fields.Char(string="Contact")
-    store = fields.Char(string="Store Location")
+    store = fields.Char(string="Pin Code")
 
     is_overdue = fields.Boolean(compute="_compute_states", store=True)
     is_closed = fields.Boolean(compute="_compute_states", store=True)
